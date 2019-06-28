ab358b81cb9f66cb2d3e30fde179a08f7d6fc394
kaplan81 <gesteira.pro@gmail.com>
2019-06-28 11:27:58 +0200

    navigation to character page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,25 +1,46 @@
-// const rickMortyApp = {};
+const rickMortyApp = {
+  urlParams: {}
+};
+
+const characters = document.querySelector('.characters');
+const character = document.querySelector('.character');
 
 init();
 
 function init() {
-  renderCharachters();
+  rickMortyApp.urlParams = new URLSearchParams(window.location.search);
+
+  if (characters) {
+    renderCharacters();
+  } else if (character) {
+    renderCharacter();
+  }
 }
 
-async function renderCharachters() {
-  const charachters = document.querySelector('.charachters');
-  const twentyFirstCharacters = await getCharacter();
-  console.log('twentyFirstCharacters:::', twentyFirstCharacters);
+async function renderCharacter() {
+  if (rickMortyApp.urlParams.has('id')) {
+    const characterId = rickMortyApp.urlParams.get('id');
+    const selectedCharacter = await getCharacter(+characterId);
+    console.log('selectedCharacter:::', selectedCharacter);
+  }
+}
 
-  for (const character of twentyFirstCharacters.results) {
+async function renderCharacters() {
+  const twentyFirstCharacters = await getCharacter();
+  // console.log('twentyFirstCharacters:::', twentyFirstCharacters);
+
+  for (const singleCharacter of twentyFirstCharacters.results) {
     const card = document.createElement('figure');
     const cardImage = document.createElement('img');
     const cardCaption = document.createElement('figcaption');
     card.classList.add('character-card');
-    cardImage.setAttribute('src', character.image);
-    cardCaption.innerHTML = `<h4>${character.name}</h4>`;
+    cardImage.setAttribute('src', singleCharacter.image);
+    cardCaption.innerHTML = `<h4>${singleCharacter.name}</h4>`;
     card.appendChild(cardImage);
     card.appendChild(cardCaption);
-    charachters.appendChild(card);
+    characters.appendChild(card);
+    card.addEventListener('click', () => {
+      window.location = `/character?id=${singleCharacter.id}`;
+    });
   }
 }
