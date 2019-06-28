c65b3f55ec2057454b85a7ce54525be3d819a84e
kaplan81 <gesteira.pro@gmail.com>
2019-06-28 12:14:00 +0200

    show character status and card in his page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,34 +12,58 @@ function init() {
 
   if (characters) {
     renderCharacters();
-  } else if (character) {
+  } else if (character && rickMortyApp.urlParams.has('id')) {
     renderCharacter();
   }
 }
 
+function createCard() {
+  const figure = document.createElement('figure');
+  const image = document.createElement('img');
+  const caption = document.createElement('figcaption');
+  figure.classList.add('character-card');
+
+  return { figure, image, caption };
+}
+
 async function renderCharacter() {
-  if (rickMortyApp.urlParams.has('id')) {
-    const characterId = rickMortyApp.urlParams.get('id');
-    const selectedCharacter = await getCharacter(+characterId);
-    console.log('selectedCharacter:::', selectedCharacter);
-  }
+  const characterId = rickMortyApp.urlParams.get('id');
+  const selectedCharacter = await getCharacter(+characterId);
+  const status =
+    selectedCharacter.status === 'Dead' || selectedCharacter.status === 'unknown'
+      ? 'dead'
+      : 'alive';
+
+  console.log('selectedCharacter:::', selectedCharacter);
+
+  const characterHeading = document.querySelector('.character-heading');
+  characterHeading.append(status);
+
+  const card = createCard();
+  card.image.setAttribute('src', selectedCharacter.image);
+  card.caption.innerHTML = `
+    <h3>${selectedCharacter.name}</h3>
+    <p>LOCATION: ${selectedCharacter.location.name}</p>
+    <p>SPECIES: ${selectedCharacter.species}</p>
+    <a href="/">Back to the list</a>
+  `;
+  card.figure.appendChild(card.image);
+  card.figure.appendChild(card.caption);
+  character.appendChild(card.figure);
 }
 
 async function renderCharacters() {
   const twentyFirstCharacters = await getCharacter();
-  // console.log('twentyFirstCharacters:::', twentyFirstCharacters);
+  console.log('twentyFirstCharacters:::', twentyFirstCharacters);
 
   for (const singleCharacter of twentyFirstCharacters.results) {
-    const card = document.createElement('figure');
-    const cardImage = document.createElement('img');
-    const cardCaption = document.createElement('figcaption');
-    card.classList.add('character-card');
-    cardImage.setAttribute('src', singleCharacter.image);
-    cardCaption.innerHTML = `<h4>${singleCharacter.name}</h4>`;
-    card.appendChild(cardImage);
-    card.appendChild(cardCaption);
-    characters.appendChild(card);
-    card.addEventListener('click', () => {
+    const card = createCard();
+    card.image.setAttribute('src', singleCharacter.image);
+    card.caption.innerHTML = `<h4>${singleCharacter.name}</h4>`;
+    card.figure.appendChild(card.image);
+    card.figure.appendChild(card.caption);
+    characters.appendChild(card.figure);
+    card.figure.addEventListener('click', () => {
       window.location = `/character?id=${singleCharacter.id}`;
     });
   }
