51da7bdb4293ee98d07666e1d36911c3f0eabbe2
kaplan81 <gesteira.pro@gmail.com>
2019-07-05 11:12:27 +0200

    install implementation

---
+++ src/scripts/install.js
@@ -0,0 +1,28 @@
+const installButton = document.querySelector('.install-btn');
+let deferredInstallPrompt = null;
+
+installButton.addEventListener('click', installPWA);
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
+function installPWA(evt) {
+  // Add code show install prompt & hide the install button.
+  deferredInstallPrompt.prompt();
+  // Hide the install button, it can't be called twice.
+  evt.srcElement.classList.add('hidden');
+  // Log user response to prompt.
+  deferredInstallPrompt.userChoice.then(choice => {
+    if (choice.outcome === 'accepted') {
+      console.log('User accepted the install prompt', choice);
+    } else {
+      evt.srcElement.classList.remove('hidden');
+      console.log('User dismissed the install prompt', choice);
+    }
+    deferredInstallPrompt = null;
+  });
+}
+
+function saveBeforeInstallPromptEvent(evt) {
+  // Add code to save event & show the install button.
+  deferredInstallPrompt = evt;
+  installButton.classList.remove('hidden');
+}
