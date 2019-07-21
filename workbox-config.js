bd1e0167aab21293f9c0fc9cba0349cdf8db7e36
kaplan81 <gesteira.pro@gmail.com>
2019-07-21 19:18:13 +0200

    add copyLibraries to build and globIgnores to workbox configs

--- workbox-config.js
+++ workbox-config.js
@@ -2,5 +2,6 @@ module.exports = {
   globDirectory: 'dist/',
   globPatterns: ['**/*.{png,jpeg,html,css,json,ico,js}'],
   swDest: 'dist/sw.js',
-  swSrc: 'src/sw-custom.js'
+  swSrc: 'src/sw-custom.js',
+  globIgnores: ['scripts/workbox-v4.3.1/**/*']
 };
